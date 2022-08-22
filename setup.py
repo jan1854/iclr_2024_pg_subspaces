a0798142087204d09965589db3ea2146e892a2ca
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-08-22 17:16:44 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='action-transformations',
+    version='',
+    packages=['action_transformations', 'action_transformations.filter', 'action_transformations.stateless'],
+    url='',
+    license='',
+    author='Jan Schneider',
+    author_email='Jan.Schneider@tuebingen.mpg.de',
+    description=''
+)
