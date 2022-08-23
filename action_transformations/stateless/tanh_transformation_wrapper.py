8dd23982336a17d69263550cbfb45be36888e78a
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-08-23 10:13:04 +0200

    Scaled the output of the TanhTransformation layer to match the original action space

--- action_transformations/stateless/tanh_transformation_wrapper.py
+++ action_transformations/stateless/tanh_transformation_wrapper.py
@@ -12,7 +12,9 @@ class TanhTransformationWrapper(ActionTransformationWrapper):
         self.scaling_pre = np.array(scaling) * np.ones(env.action_space.shape)    # Copy to array if scalar
         assert isinstance(env.action_space, gym.spaces.Box)
         self.offset = 0.5 * (env.action_space.low + env.action_space.high)
-        self.scaling_post = 0.5 * (self.action_space.high - self.action_space.low)
+        tanh_output_range = np.tanh(env.action_space.high * self.scaling_pre) \
+                            - np.tanh(env.action_space.low * self.scaling_pre)
+        self.scaling_post = (self.action_space.high - self.action_space.low) / tanh_output_range
 
     def get_transformation_parameters(self) -> np.ndarray:
         return self.scaling_pre
@@ -21,4 +23,4 @@ class TanhTransformationWrapper(ActionTransformationWrapper):
         self.scaling_pre = transformation_parameters
 
     def transform_action(self, action: np.ndarray) -> np.ndarray:
-        return np.tanh(action * self.scaling_pre) * self.scaling_post + self.offset
+        return np.tanh((action - self.offset) * self.scaling_pre) * self.scaling_post + self.offset
