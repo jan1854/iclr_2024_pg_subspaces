8e2f174e945d71e379f73df205090f3cd36253b6
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-10-06 18:17:27 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name='action-space-toolbox',
     version='',
-    packages=['action_space_toolbox', 'action_space_toolbox.filter', 'action_space_toolbox.stateless'],
+    packages=['action_space_toolbox', 'action_space_toolbox.util', 'action_space_toolbox.control_modes',
+              'action_space_toolbox.transformations', 'action_space_toolbox.transformations.filter',
+              'action_space_toolbox.transformations.stateless'],
     url='',
     license='',
     author='Jan Schneider',
