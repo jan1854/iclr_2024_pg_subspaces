e552512c0dca03f78d6a23156ac725d64070707e
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-10-07 14:54:22 +0200

    Added requirements also to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='action-space-toolbox',
-    version='',
-    packages=['action_space_toolbox', 'action_space_toolbox.util', 'action_space_toolbox.control_modes',
-              'action_space_toolbox.transformations', 'action_space_toolbox.transformations.filter',
-              'action_space_toolbox.transformations.stateless'],
-    url='',
-    license='',
-    author='Jan Schneider',
-    author_email='Jan.Schneider@tuebingen.mpg.de',
-    description=''
+    name="action-space-toolbox",
+    version="",
+    packages=["action_space_toolbox", "action_space_toolbox.util", "action_space_toolbox.control_modes",
+              "action_space_toolbox.transformations", "action_space_toolbox.transformations.filter",
+              "action_space_toolbox.transformations.stateless"],
+    url="",
+    license="",
+    author="Jan Schneider",
+    author_email="Jan.Schneider@tuebingen.mpg.de",
+    description="",
+    install_requires=[
+        "gym",
+        "numpy"
+    ]
 )
