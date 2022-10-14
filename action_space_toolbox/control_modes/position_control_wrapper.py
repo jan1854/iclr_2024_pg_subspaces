71c74e5747d3a98e2a66f69a0c5a8db059ea3818
Jan Schneider <Jan.Schneider1997@gmail.com>
2022-10-14 22:54:32 +0200

    Added the option to make the position controller relative to the current position

--- action_space_toolbox/control_modes/position_control_wrapper.py
+++ action_space_toolbox/control_modes/position_control_wrapper.py
@@ -16,6 +16,7 @@ class PositionControlWrapper(ActionTransformationWrapper):
         env: ControllerBaseEnv,
         p_gains: Union[float, np.ndarray] = 1.0,
         d_gains: Union[float, np.ndarray] = 1.0,
+        positions_relative: bool = False,
     ):
         super().__init__(env)
         if np.isscalar(p_gains):
@@ -25,10 +26,18 @@ class PositionControlWrapper(ActionTransformationWrapper):
         assert p_gains.shape == d_gains.shape == env.action_space.shape
         self.p_gains = p_gains
         self.d_gains = d_gains
-        self.action_space = gym.spaces.Box(
-            env.dof_pos_bounds[:, 0].astype(np.float32),
-            env.dof_pos_bounds[:, 1].astype(np.float32),
-        )
+        self.positions_relative = positions_relative
+        if not positions_relative:
+            # TODO: This assumes revolute joints with no joint limits
+            self.action_space = gym.spaces.Box(
+                -np.pi * np.ones(env.action_space.shape, dtype=np.float32),
+                np.pi * np.ones(env.action_space.shape, dtype=np.float32),
+            )
+        else:
+            self.action_space = gym.spaces.Box(
+                env.dof_pos_bounds[:, 0].astype(np.float32),
+                env.dof_pos_bounds[:, 1].astype(np.float32),
+            )
 
     def transform_action(self, action: np.ndarray) -> np.ndarray:
         pos = self.dof_positions
@@ -36,7 +45,10 @@ class PositionControlWrapper(ActionTransformationWrapper):
         # Normalize the angle distances of all revolute dofs (so that diff(pi - 0.1, -pi + 0.1) = 0.2 not 2pi - 0.2)
         # TODO: This only works if there are no angle bounds in place and the joint can do an arbitrary number of
         #       rotations
-        pos_error = ~self.dofs_revolute * (
-            pos - action
-        ) + self.dofs_revolute * normalize_angle(pos - action)
+        if self.positions_relative:
+            pos_error = action
+        else:
+            pos_error = ~self.dofs_revolute * (
+                pos - action
+            ) + self.dofs_revolute * normalize_angle(pos - action)
         return -self.p_gains * pos_error - self.d_gains * vel
