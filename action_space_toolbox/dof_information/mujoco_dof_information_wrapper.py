cfb55394b6d5b7712e5b1e984e68ee48cebec9ff
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-10-17 18:46:35 +0200

    Handled the case that all joints are not limited and revolute

--- action_space_toolbox/dof_information/mujoco_dof_information_wrapper.py
+++ action_space_toolbox/dof_information/mujoco_dof_information_wrapper.py
@@ -11,13 +11,19 @@ class MujocoDofInformationWrapper(DofInformationWrapper):
     def __init__(self, env: gym.Env):
         assert isinstance(env.unwrapped, MujocoEnv)
         self.actuated_joints = env.model.actuator_trnid[:, 0]
-        dof_pos_bounds = (
-            env.model.jnt_range[self.actuated_joints]
-            if np.all(env.model.jnt_limited[self.actuated_joints])
-            else None
-        )
-        dof_vel_bounds = None  # TODO: There does not appear to be a maximum joint velocity property of the Mujoco environments
         dofs_revolute = env.model.jnt_type[self.actuated_joints] == 3
+        # TODO: This should support mixed limited / unlimited joints
+        if np.all(env.model.jnt_limited[self.actuated_joints]):
+            dof_pos_bounds = env.model.jnt_range[self.actuated_joints]
+        elif not np.any(env.model.jnt_limited[self.actuated_joints]) and np.all(
+            dofs_revolute
+        ):
+            dof_pos_bounds = np.repeat(
+                np.array([[-np.pi, np.pi]]), env.action_space.shape[0], axis=0
+            )
+        else:
+            dof_pos_bounds = None
+        dof_vel_bounds = None  # TODO: There does not appear to be a maximum joint velocity property of the Mujoco environments
         super().__init__(env, dof_pos_bounds, dof_vel_bounds, dofs_revolute)
 
     @property
