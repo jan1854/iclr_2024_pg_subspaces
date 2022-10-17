3956900a22e812ca4059724c87cf7a331fd5eb7c
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-10-17 17:22:26 +0200

    Formatted test_control_modes with black

--- test/test_control_modes.py
+++ test/test_control_modes.py
@@ -7,7 +7,9 @@ from action_space_toolbox.util.angles import normalize_angle
 def test_position_control_pendulum():
     env = gym.make("Pendulum_PC-v1")
     env.reset()
-    target_positions = [np.pi - d for d in np.arange(0.0, 0.5, 0.1)] + [-np.pi + d for d in np.arange(0.1, 0.5, 0.1)]
+    target_positions = [np.pi - d for d in np.arange(0.0, 0.5, 0.1)] + [
+        -np.pi + d for d in np.arange(0.1, 0.5, 0.1)
+    ]
     for target_position in target_positions:
         for _ in range(100):
             env.step(target_position)
@@ -17,7 +19,10 @@ def test_position_control_pendulum():
 
 
 def test_velocity_control_pendulum():
-    env = gym.make("Pendulum_VC-v1", g=0.0)    # Disable gravity, so that we can achieve the velocity at all times
+    env = gym.make(
+        "Pendulum_VC-v1",
+        g=0.0,  # Disable gravity, so that we can achieve the velocity at all times
+    )
     env.reset()
     target_velocities = np.arange(-0.2, 0.2, 0.1)
     for target_velocity in target_velocities:
