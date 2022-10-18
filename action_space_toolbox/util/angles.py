600fac15698643f4a0bf598298b912fc337d0258
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-10-18 18:46:32 +0200

    Fixed array compatibility of normalize_angle

--- action_space_toolbox/util/angles.py
+++ action_space_toolbox/util/angles.py
@@ -5,6 +5,5 @@ import numpy as np
 
 def normalize_angle(a: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
     a = a % (2 * np.pi)
-    if a > np.pi:
-        a -= 2 * np.pi
+    a = np.where(a > np.pi, a - 2 * np.pi, a)
     return a
--- test/test_util.py
+++ test/test_util.py
@@ -12,3 +12,6 @@ def test_angle_normalization():
     assert normalize_angle(-np.pi - 0.05) == pytest.approx(np.pi - 0.05)
     assert normalize_angle(4 * np.pi) == pytest.approx(0.0)
     assert normalize_angle(3 * np.pi) == pytest.approx(np.pi)
+    assert normalize_angle(
+        np.array([0.5 * np.pi, 1.2 * np.pi, -1.3 * np.pi])
+    ) == pytest.approx(np.array([0.5 * np.pi, -0.8 * np.pi, 0.7 * np.pi]))
