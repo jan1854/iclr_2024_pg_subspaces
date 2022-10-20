5f7cba10b06ee83f4aafce956b1eb58358fb03bf
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-10-20 09:35:29 +0200

    Added a testcase for the multiturn angle difference calculation

--- test/test_control_modes.py
+++ test/test_control_modes.py
@@ -1,6 +1,8 @@
 import gym
 import numpy as np
+from _pytest.python_api import approx
 
+from action_space_toolbox import PositionControlWrapper, DofInformationWrapper
 from action_space_toolbox.util.angles import normalize_angle
 
 
@@ -31,3 +33,33 @@ def test_velocity_control_pendulum():
         for _ in range(10):
             env.step(target_velocity)
             assert np.all(np.abs(env.dof_velocities[0] - target_velocity).item() < 0.1)
+
+
+def test_position_control_multiturn():
+    class DummyEnv:
+        action_space = gym.spaces.Box(
+            np.array([-10, -10], dtype=np.float32),
+            np.array([10, 10], dtype=np.float32),
+        )
+
+    class DummyDofInformationWrapper(DofInformationWrapper):
+        def __init__(self, env: gym.Env):
+            dofs_revolute = np.array([True, True])
+            super().__init__(
+                env,
+                np.array([[-np.pi, np.pi], [-3, 3]]),
+                np.array([[-1, -1], [1, 1]]),
+                dofs_revolute,
+            )
+
+        @property
+        def dof_positions(self) -> np.ndarray:
+            return np.array([np.pi - 0.1, 2.9])
+
+        @property
+        def dof_velocities(self) -> np.ndarray:
+            return np.zeros(2)
+
+    env = PositionControlWrapper(DummyDofInformationWrapper(DummyEnv()), p_gains=1.0, d_gains=0.0)  # type: ignore
+    pos_diff = env.transform_action(np.array([-np.pi + 0.1, -2.9]))
+    assert pos_diff == approx(np.array([0.2, -5.8]))
