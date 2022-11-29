1dec906a2547c8a842cdf8359fc324b9867c2d08
Jan Schneider <Jan.Schneider@tuebingen.mpg.de>
2022-11-29 10:00:59 +0100

    Moved the combine_metrics script to action_space_toolbox

---
+++ action_space_toolbox/scripts/combine_metrics.py
@@ -0,0 +1,89 @@
+import argparse
+import shutil
+from pathlib import Path
+from typing import Sequence, Any, Dict
+
+import yaml
+from tqdm import tqdm
+
+from action_space_optimization.util.tensorboard_logs import combine_tb_logs
+
+
+def same_configurations(run_dirs: Sequence[Path]) -> bool:
+    configs = []
+    for run_dir in run_dirs:
+        with (run_dir / ".hydra" / "config.yaml").open("r") as config_file:
+            configs.append(yaml.safe_load(config_file))
+
+    for config in configs[1:]:
+        if not config_equal(configs[0], config, ["seed"]):
+            return False
+    return True
+
+
+def config_equal(
+    c1: Dict[str, Any], c2: Dict[str, Any], exception_keys: Sequence[str] = ()
+) -> bool:
+    if len(c1) != len(c2):
+        return False
+    for k, v1 in c1.items():
+        if k not in c2:
+            return False
+        elif k not in exception_keys:
+            if isinstance(v1, Dict):
+                if not isinstance(c2[k], Dict) or not config_equal(
+                    v1, c2[k], exception_keys
+                ):
+                    return False
+            else:
+                if v1 != c2[k]:
+                    return False
+    return True
+
+
+def combine_metrics(log_path: Path) -> None:
+    jobs = []
+    training_log_path = log_path / "training"
+    for env_dir in training_log_path.iterdir():
+        for date_dir in env_dir.iterdir():
+            for experiment_dir in date_dir.iterdir():
+                run_dirs = [
+                    d
+                    for d in experiment_dir.iterdir()
+                    if d.is_dir() and d.name.isnumeric()
+                ]
+                all_done = True
+                for curr_run_dir in run_dirs:
+                    if curr_run_dir.exists():
+                        all_done &= (curr_run_dir / "done").exists()
+                    else:
+                        break
+                combined_dir = experiment_dir / "combined"
+                meta_path = combined_dir / ".meta.yaml"
+                if meta_path.exists():
+                    with meta_path.open("r") as meta_file:
+                        meta = yaml.safe_load(meta_file)
+                        update = (
+                            meta["num_runs"] < len(run_dirs) or not meta["all_done"]
+                        )
+                else:
+                    update = True
+
+                if len(run_dirs) > 1 and same_configurations(run_dirs) and update:
+                    jobs.append((run_dirs, combined_dir, all_done))
+
+    for run_dirs, combined_dir, all_done in tqdm(jobs):
+        tb_dirs = [run_dir / "tensorboard" for run_dir in run_dirs]
+        if combined_dir.exists():
+            shutil.rmtree(combined_dir)
+        combine_tb_logs(tb_dirs, combined_dir)
+        with (combined_dir / ".meta.yaml").open("w") as meta_file:
+            yaml.dump({"num_runs": len(tb_dirs), "all_done": all_done}, meta_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("log_path", type=str)
+    args = parser.parse_args()
+
+    combine_metrics(Path(args.log_path))
