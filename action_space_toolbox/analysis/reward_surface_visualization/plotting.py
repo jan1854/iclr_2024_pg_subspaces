Fixed a bug in the transformation of the gradient steps and saved also the plain sgd gradient steps

action_space_toolbox/analysis/reward_surface_visualization/plotting.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import PIL.Image
 import numpy as np
 import plotly.graph_objects
-
 from PIL import Image
 from scipy.interpolate import RegularGridInterpolator
 
@@ -19,11 +18,19 @@ PLOT_NAME_TO_TITLE_DESCR = {
 }
 
 
-def plot_all_results(analysis_dir: Path, overwrite=False) -> TensorboardLogs:
+def plot_results(
+    analysis_dir: Path,
+    step: int,
+    plot_num: int,
+    overwrite: bool = False,
+    plot_sgd_steps: bool = False,
+) -> TensorboardLogs:
     logs = TensorboardLogs()
 
     for plot_name, plot_title in PLOT_NAME_TO_TITLE_DESCR.items():
-        for results_path in (analysis_dir / plot_name / "data").iterdir():
+        for results_path in (analysis_dir / plot_name / "data").glob(
+            f"*{step:07d}_{plot_num:02d}*"
+        ):
             plot_path = results_path.parents[1] / results_path.with_suffix("").name
 
             if not plot_path.exists() or overwrite:
@@ -31,6 +38,11 @@ def plot_all_results(analysis_dir: Path, overwrite=False) -> TensorboardLogs:
                     results = pickle.load(results_file)
                 data = results["data"]
 
+                sgd_steps = (
+                    results.get("sampled_projected_sgd_steps", [])
+                    if plot_sgd_steps
+                    else []
+                )
                 plot_surface(
                     results["magnitude"],
                     data,
@@ -39,7 +51,8 @@ def plot_all_results(analysis_dir: Path, overwrite=False) -> TensorboardLogs:
                     results["env_step"],
                     results["plot_num"],
                     plot_title,
-                    results.get("sampled_projected_gradient_steps", []),
+                    results.get("sampled_projected_optimizer_steps", []),
+                    sgd_steps,
                     logs,
                     analysis_dir.name,
                     plot_path,
@@ -55,7 +68,8 @@ def plot_surface(
     env_step: int,
     plot_nr: int,
     title_descr: str,
-    projected_gradient_steps: np.ndarray,
+    projected_optimizer_steps: np.ndarray,
+    projected_sgd_steps: np.ndarray,
     logs: TensorboardLogs,
     analysis_run_id: str,
     outpath: Path,
@@ -86,18 +100,31 @@ def plot_surface(
         y=[0.0, 0.0],
         z=[np.min(results) - 0.1 * Z_range, np.max(results) + 0.1 * Z_range],
         mode="lines",
-        line_width=8,
+        line_width=10,
         line_color="black",
         showlegend=False,
     )
     interpolator = RegularGridInterpolator((coords, coords), results, method="linear")
 
-    for grad_step in projected_gradient_steps:
+    for grad_step in projected_sgd_steps:
+        visualization_steps = np.linspace(np.zeros(2), -3e-4 * grad_step, 20)
+        fig.add_scatter3d(
+            x=visualization_steps[:, 1],
+            y=visualization_steps[:, 0],
+            z=interpolator(visualization_steps) + 0.001 * Z_range,
+            mode="lines",
+            line_width=6,
+            line_color="blue",
+            showlegend=False,
+            opacity=0.1,
+        )
+
+    for grad_step in projected_optimizer_steps:
         # Make sure that the gradient step does not point outside the grid (otherwise the interpolation will throw an
         # error). Scale the gradient step to reduce the length but keep the direction the same.
-        if np.max(grad_step) > magnitude:
-            grad_step = grad_step * magnitude / np.max(grad_step) - 1e-6
-        visualization_steps = np.linspace(np.zeros(2), grad_step, 100)
+        if np.max(np.abs(grad_step)) > magnitude:
+            grad_step = grad_step * magnitude / np.max(np.abs(grad_step)) - 1e-8
+        visualization_steps = np.linspace(np.zeros(2), grad_step, 20)
         # TODO: Check out the "Setting Angle Reference" example at https://plotly.com/python/marker-style
         fig.add_scatter3d(
             x=visualization_steps[:, 1],
@@ -113,9 +140,9 @@ def plot_surface(
     if title is not None:
         fig.update_layout(title=title)
 
-    fig.write_image(outpath.with_suffix(".png"), scale=5)
+    fig.write_image(outpath.with_suffix(".png"), scale=2)
     fig.write_html(outpath.with_suffix(".html"))
     with Image.open(outpath.with_suffix(".png")) as im:
         # Make the image smaller so that it fits better in tensorboard
-        im = im.resize((im.width // 5, im.height // 5), PIL.Image.Resampling.LANCZOS)
+        im = im.resize((im.width // 2, im.height // 2), PIL.Image.Resampling.LANCZOS)
         logs.add_image(f"{plot_name}/{analysis_run_id}/{plot_nr}", im, env_step)

action_space_toolbox/analysis/reward_surface_visualization/reward_surface_visualization.py
@@ -3,7 +3,7 @@ import logging
 import pickle
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Sequence, Union
+from typing import Callable, Sequence, Union, Tuple
 
 import gym
 import numpy as np
@@ -15,7 +15,7 @@ from tqdm import tqdm
 
 from action_space_toolbox.analysis.analysis import Analysis
 from action_space_toolbox.analysis.reward_surface_visualization.plotting import (
-    plot_all_results,
+    plot_results,
 )
 from action_space_toolbox.util.get_episode_length import get_episode_length
 from action_space_toolbox.util.sb3_training import fill_rollout_buffer, ppo_loss
@@ -150,9 +150,10 @@ class RewardSurfaceVisualization(Analysis):
                 )
             ]
 
-            sampled_projected_gradient_steps = self.sample_projected_gradient_steps(
-                direction1, direction2, 10
-            )
+            (
+                projected_optimizer_steps,
+                projected_sgd_steps,
+            ) = self.sample_projected_gradient_steps(direction1, direction2, 10)
 
             plot_info = {
                 "env_name": agent.env.envs[0].spec.id,
@@ -163,7 +164,8 @@ class RewardSurfaceVisualization(Analysis):
                     [d.cpu().numpy() for d in direction1],
                     [d.cpu().numpy() for d in direction2],
                 ),
-                "sampled_projected_gradient_steps": sampled_projected_gradient_steps,
+                "sampled_projected_optimizer_steps": projected_optimizer_steps,
+                "sampled_projected_sgd_steps": projected_sgd_steps,
             }
 
             rewards_undiscounted = np.array(
@@ -212,8 +214,10 @@ class RewardSurfaceVisualization(Analysis):
             logs.update(
                 process_pool.apply(
                     functools.partial(
-                        plot_all_results,
+                        plot_results,
                         self.out_dir,
+                        step=env_step,
+                        plot_num=plot_num,
                         overwrite=overwrite_results,
                     )
                 )
@@ -325,10 +329,19 @@ class RewardSurfaceVisualization(Analysis):
         direction1: Sequence[torch.Tensor],
         direction2: Sequence[torch.Tensor],
         num_samples: int,
-    ) -> np.ndarray:
-        projected_parameters = []
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        projected_optimizer_parameters = []
+        projected_sgd_parameters = []
+
+        direction1_vec = torch.cat([d.flatten() for d in direction1])
+        direction2_vec = torch.cat([d.flatten() for d in direction2])
+        directions = torch.stack((direction1_vec, direction2_vec), dim=1).double()
+
         for _ in range(num_samples):
             agent = self.agent_factory(self.env_factory())
+            old_parameters = torch.cat(
+                [p.flatten().clone() for p in agent.policy.parameters()]
+            )
             rollout_buffer_gradient_step = (
                 stable_baselines3.common.buffers.RolloutBuffer(
                     agent.n_steps,
@@ -345,19 +358,32 @@ class RewardSurfaceVisualization(Analysis):
             )
             agent.policy.zero_grad()
             loss.backward()
+            gradient = torch.cat([p.grad.flatten() for p in agent.policy.parameters()])
+            new_sgd_parameters = (
+                old_parameters - agent.policy.optimizer.param_groups[0]["lr"] * gradient
+            )
             agent.policy.optimizer.step()
-            direction1_vec = torch.cat([d.flatten() for d in direction1])
-            direction2_vec = torch.cat([d.flatten() for d in direction2])
-            directions = torch.stack((direction1_vec, direction2_vec), dim=1)
-            new_parameters = torch.cat([p.flatten() for p in agent.policy.parameters()])
+            new_optimizer_parameters = torch.cat(
+                [p.flatten() for p in agent.policy.parameters()]
+            )
             # Projection matrix: (directions^T @ directions)^(-1) @ directions^T
-            curr_projected_parameters = torch.linalg.solve(
-                directions.T @ directions, directions.T @ new_parameters
+            curr_projected_optimizer_parameters = torch.linalg.solve(
+                directions.T @ directions,
+                directions.T @ (new_optimizer_parameters - old_parameters).double(),
             )
-            projected_parameters.append(
-                curr_projected_parameters.detach().cpu().numpy()
+            curr_projected_sgd_parameters = torch.linalg.solve(
+                directions.T @ directions,
+                directions.T @ (new_sgd_parameters - old_parameters).double(),
             )
-        return np.stack(projected_parameters)
+            projected_optimizer_parameters.append(
+                curr_projected_optimizer_parameters.detach().cpu().numpy()
+            )
+            projected_sgd_parameters.append(
+                curr_projected_sgd_parameters.detach().cpu().numpy()
+            )
+        return np.stack(projected_optimizer_parameters), np.stack(
+            projected_sgd_parameters
+        )
 
     @staticmethod
     def _result_filename(plot_name: str, env_step: int, plot_idx: int) -> str:

action_space_toolbox/scripts/create_reward_surface_plots.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from torch.utils.tensorboard import SummaryWriter
 
 from action_space_toolbox.analysis.reward_surface_visualization.plotting import (
-    plot_all_results,
+    plot_results,
 )
 
 
@@ -12,17 +12,20 @@ def create_reward_surface_plots(
     analysis_dir: Path,
     summary_writer: SummaryWriter,
 ) -> None:
-    logs = plot_all_results(analysis_dir, overwrite=True)
-    logs.log(summary_writer)
+    for data_file in (analysis_dir / "loss_surface" / "data").iterdir():
+        step = int(data_file.name[-14:-7])
+        plot_num = int(data_file.name[-6:-4])
+        logs = plot_results(analysis_dir, step, plot_num, overwrite=True)
+        logs.log(summary_writer)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("logdir", type=str)
+    parser.add_argument("--id", type=str, default="default")
     args = parser.parse_args()
 
     logdir = Path(args.logdir)
-    analysis_dir = logdir / "analyses" / "reward_surface_visualization"
-    for curr_analysis_dir in analysis_dir.iterdir():
-        summary_writer = SummaryWriter(str(logdir / "tensorboard"))
-        create_reward_surface_plots(curr_analysis_dir, summary_writer)
+    analysis_dir = logdir / "analyses" / "reward_surface_visualization" / args.id
+    summary_writer = SummaryWriter(str(logdir / "tensorboard"))
+    create_reward_surface_plots(analysis_dir, summary_writer)