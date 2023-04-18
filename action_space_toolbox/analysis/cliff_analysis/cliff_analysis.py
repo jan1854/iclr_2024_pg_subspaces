Fixed a bug where the CliffAnalysis followed the gradient in the wrong direction and implemented the option to use the gradient normalization of the Cliff Diving paper

action_space_toolbox/analysis/cliff_analysis/cliff_analysis.py
@@ -20,6 +20,7 @@ from action_space_toolbox.analysis.util import (
     ReturnEvaluationResult,
     evaluate_agent_losses,
     LossEvaluationResult,
+    filter_normalize_direction,
 )
 from action_space_toolbox.util.agent_spec import AgentSpec, HydraAgentSpec
 from action_space_toolbox.util.get_episode_length import get_episode_length
@@ -44,6 +45,7 @@ class CliffAnalysis(Analysis):
         num_trials: int,
         num_episodes_reward_eval: int,
         num_env_steps_training: int,
+        original_gradient_normalization: bool,
         cliff_test_distance: float,
         alternate_agent_cfg: Optional[str],
         algorithm_overrides: Dict[str, Sequence[Dict[str, Any]]],
@@ -59,6 +61,7 @@ class CliffAnalysis(Analysis):
         self.num_trials = num_trials
         self.num_episodes_reward_eval = num_episodes_reward_eval
         self.num_env_steps_training = num_env_steps_training
+        self.original_gradient_normalization = original_gradient_normalization
         self.cliff_test_distance = cliff_test_distance
         self.results_dir = run_dir / "analyses" / "cliff_analysis" / analysis_run_id
         self.results_dir.mkdir(parents=True, exist_ok=True)
@@ -136,8 +139,17 @@ class CliffAnalysis(Analysis):
 
         if overwrite_results or not self._check_logs_complete(results, env_step):
             gradient, _, _ = ppo_gradient(agent, next(rollout_buffer_true_loss.get()))
-            gradient_norm = torch.norm(flatten_parameters(gradient))
-            normalized_gradient = [g / gradient_norm for g in gradient]
+            if self.original_gradient_normalization:
+                filter_normalized_gradient = filter_normalize_direction(
+                    gradient, [p.detach() for p in agent.policy.parameters()]
+                )
+                # This is not the L1 norm (missing the abs), not sure why the Cliff Diving authors use this
+                # normalization
+                grad_sum = torch.sum(flatten_parameters(filter_normalized_gradient))
+                normalized_gradient = [g / grad_sum for g in filter_normalized_gradient]
+            else:
+                gradient_norm = torch.norm(flatten_parameters(gradient))
+                normalized_gradient = [g / gradient_norm for g in gradient]
 
             reward_checkpoint = evaluate_returns_rollout_buffer(
                 rollout_buffer_true_loss_no_value_bootstrap,
@@ -152,7 +164,7 @@ class CliffAnalysis(Analysis):
             }
 
             cliff_test_parameters = [
-                p + self.cliff_test_distance * g
+                p - self.cliff_test_distance * g
                 for p, g in zip(agent.policy.parameters(), normalized_gradient)
             ]
             reward_cliff_test = evaluate_agent_returns(

action_space_toolbox/analysis/reward_surface_visualization/reward_surface_visualization.py
@@ -2,7 +2,7 @@ import functools
 import logging
 import pickle
 from pathlib import Path
-from typing import Callable, Iterable, Optional, Sequence, Tuple
+from typing import Callable, Iterable, List, Optional, Sequence, Tuple
 
 import gym
 import numpy as np
@@ -20,6 +20,7 @@ from action_space_toolbox.analysis.util import (
     flatten_parameters,
     evaluate_agent_returns,
     evaluate_agent_losses,
+    filter_normalize_direction,
 )
 from action_space_toolbox.util.agent_spec import AgentSpec
 from action_space_toolbox.util.sb3_training import (
@@ -126,10 +127,9 @@ class RewardSurfaceVisualization(Analysis):
                 num_spawned_processes=self.num_processes,
             )
 
-            direction2 = [
-                self.sample_filter_normalized_direction(p.detach())
-                for p in agent.policy.parameters()
-            ]
+            direction2 = self.sample_filter_normalized_direction(
+                list(agent.policy.parameters())
+            )
 
             if self.plot_in_gradient_direction:
                 # Normalize the gradient to have the same length as the random direction vector (as described in
@@ -146,10 +146,9 @@ class RewardSurfaceVisualization(Analysis):
                 )
                 direction1 = [g / gradient_norm * direction2_norm for g in gradient]
             else:
-                direction1 = [
-                    self.sample_filter_normalized_direction(p.detach())
-                    for p in agent.policy.parameters()
-                ]
+                direction1 = self.sample_filter_normalized_direction(
+                    list(agent.policy.parameters())
+                )
 
             agent_weights = [p.detach().clone() for p in agent.policy.parameters()]
             weights_offsets = [[None] * self.grid_size for _ in range(self.grid_size)]
@@ -329,31 +328,14 @@ class RewardSurfaceVisualization(Analysis):
         return logs
 
     @classmethod
-    def sample_filter_normalized_direction(cls, param: torch.Tensor) -> torch.Tensor:
-        ndims = len(param.shape)
-        if ndims == 1 or ndims == 0:
-            # don't do any random direction for scalars
-            return torch.zeros_like(param)
-        elif ndims == 2:
-            direction = torch.normal(0.0, 1.0, size=param.shape, device=param.device)
-            direction /= torch.sqrt(
-                torch.sum(torch.square(direction), dim=0, keepdim=True)
-            )
-            direction *= torch.sqrt(torch.sum(torch.square(param), dim=0, keepdim=True))
-            return direction
-        elif ndims == 4:
-            direction = torch.normal(0.0, 1.0, size=param.shape, device=param.device)
-            direction /= torch.sqrt(
-                torch.sum(torch.square(direction), dim=(0, 1, 2), keepdim=True)
-            )
-            direction *= torch.sqrt(
-                torch.sum(torch.square(param), dim=(0, 1, 2), keepdim=True)
-            )
-            return direction
-        else:
-            raise ValueError(
-                f"Only 1, 2, 4 dimensional filters allowed, got {param.shape}."
-            )
+    def sample_filter_normalized_direction(
+        cls,
+        param: Sequence[torch.Tensor],
+    ) -> List[torch.Tensor]:
+        direction = [
+            torch.normal(0.0, 1.0, size=p.shape, device=p.device) for p in param
+        ]
+        return filter_normalize_direction(direction, [p.detach() for p in param])
 
     def sample_projected_update_trajectories(
         self,

action_space_toolbox/analysis/util.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import Callable, Iterable, Optional, Sequence, Union
+from typing import Callable, Iterable, List, Optional, Sequence, Union
 
 import gym
 import numpy as np
@@ -204,3 +204,38 @@ def evaluate_agent_losses(
 
 def flatten_parameters(seq: Sequence[torch.Tensor]) -> torch.Tensor:
     return torch.cat([s.flatten() for s in seq])
+
+
+def filter_normalize_direction(
+    direction: Sequence[torch.Tensor], parameters: Sequence[torch.Tensor]
+) -> List[torch.Tensor]:
+    return [normalize_filter(d, p) for d, p in zip(direction, parameters)]
+
+
+def normalize_filter(
+    filter_direction: torch.Tensor, filter_parameters: torch.Tensor
+) -> torch.Tensor:
+    ndims = len(filter_parameters.shape)
+    if ndims == 1 or ndims == 0:
+        # don't do any random direction for scalars
+        return torch.zeros_like(filter_parameters)
+    elif ndims == 2:
+        normalized_direction = filter_direction / torch.sqrt(
+            torch.sum(torch.square(filter_direction), dim=0, keepdim=True)
+        )
+        normalized_direction *= torch.sqrt(
+            torch.sum(torch.square(filter_parameters), dim=0, keepdim=True)
+        )
+        return normalized_direction
+    elif ndims == 4:
+        normalized_direction = filter_direction / torch.sqrt(
+            torch.sum(torch.square(filter_direction), dim=(0, 1, 2), keepdim=True)
+        )
+        normalized_direction *= torch.sqrt(
+            torch.sum(torch.square(filter_parameters), dim=(0, 1, 2), keepdim=True)
+        )
+        return normalized_direction
+    else:
+        raise ValueError(
+            f"Only 1, 2, 4 dimensional filters allowed, got {filter_parameters.shape}."
+        )