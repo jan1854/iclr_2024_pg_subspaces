008fefd5331d92357c57b797231ec6682eed35f9
Jan Schneider <Jan.Schneider1997@gmail.com>
2023-10-12 13:53:06 +0200

    Added the option to plot multiple experiment configurations

--- pg_subspaces/scripts/create_all_learning_curves.py
+++ pg_subspaces/scripts/create_all_learning_curves.py
@@ -74,7 +74,7 @@ def worker(
         )
     except Exception as e:
         logger.warning(
-            f"Could not create plot {out.relative_to(out.parents[2])}, got exception {e}"
+            f"Could not create plot {out.relative_to(out.parents[2])}, got exception: {e}"
         )
 
 
@@ -87,6 +87,10 @@ if __name__ == "__main__":
         "r"
     ) as run_configs_file:
         run_configs = yaml.safe_load(run_configs_file)
+    with (Path(__file__).parent / "res" / "experiment_configs.yaml").open(
+        "r"
+    ) as experiment_configs_file:
+        experiment_configs = yaml.safe_load(experiment_configs_file)
 
     results = []
     with multiprocessing.Pool(20) as pool:
@@ -94,44 +98,57 @@ if __name__ == "__main__":
             env_file_name = env_name[:-3].lower().replace("-", "_")
             if not env_name.startswith("dmc"):
                 env_file_name = "gym_" + env_file_name
-            for out_filename, plot_config in PLOT_CONFIGS.items():
-                out_path = (
-                    out_dir
-                    / plot_config["out_dir"]
-                    / f"{env_file_name}_{out_filename}.pdf"
-                )
-                out_path.parent.mkdir(exist_ok=True, parents=True)
-                title_env_name = (
-                    env_name[4:-3] if env_name.startswith("dmc_") else env_name[:-3]
-                )
-                results.append(
-                    pool.apply_async(
-                        worker,
-                        (
-                            [
-                                Path(log_dir, "training", env_name, experiment_dir)
-                                for experiment_dir in run_config["log_dirs"].values()
-                            ],
-                            [
-                                algo_name.upper()
-                                for algo_name in run_config["log_dirs"].keys()
-                            ],
-                            plot_config.get("title"),
-                            plot_config.get("xlabel", "Environment steps"),
-                            plot_config["ylabel"],
-                            (run_config.get("xmin", 0), run_config.get("xmax")),
-                            (plot_config.get("ymin"), plot_config.get("ymax")),
-                            False,
-                            plot_config["key"],
-                            plot_config["smoothing_weight"],
-                            True,
-                            True,
-                            plot_config.get("num_same_color_plots", 1),
-                            plot_config.get("fontsize", 22),
-                            plot_config.get("linewidth", 1.5),
-                            out_path,
-                        ),
+            for out_experiment_dir, algorithms in experiment_configs.items():
+                for (
+                    out_filename,
+                    plot_config,
+                ) in PLOT_CONFIGS.items():
+                    out_path = (
+                        out_dir
+                        / out_experiment_dir
+                        / plot_config["out_dir"]
+                        / f"{env_file_name}_{out_filename}"
                     )
-                )
+                    out_path.parent.mkdir(exist_ok=True, parents=True)
+                    title_env_name = (
+                        env_name[4:-3] if env_name.startswith("dmc_") else env_name[:-3]
+                    )
+                    results.append(
+                        pool.apply_async(
+                            worker,
+                            (
+                                [
+                                    Path(
+                                        log_dir,
+                                        "training",
+                                        env_name,
+                                        run_config["log_dirs"][algo_name],
+                                    )
+                                    if algo_name in run_config["log_dirs"]
+                                    else None
+                                    for algo_name in algorithms.keys()
+                                ],
+                                [
+                                    algo_descr
+                                    for algo_name, algo_descr in algorithms.items()
+                                    if algo_name in run_config["log_dirs"]
+                                ],
+                                plot_config.get("title"),
+                                plot_config.get("xlabel", "Environment steps"),
+                                plot_config["ylabel"],
+                                (run_config.get("xmin", 0), run_config.get("xmax")),
+                                (plot_config.get("ymin"), plot_config.get("ymax")),
+                                False,
+                                plot_config["key"],
+                                plot_config["smoothing_weight"],
+                                True,
+                                True,
+                                plot_config.get("num_same_color_plots", 1),
+                                plot_config.get("fontsize", 22),
+                                plot_config.get("linewidth", 1.5),
+                                out_path,
+                            ),
+                        )
+                    )
         for res in tqdm(results):
             res.get()
