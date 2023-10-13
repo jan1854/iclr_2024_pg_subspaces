ddd6d4c63f3cb2e988dadb348dc164d4e9d40261
Jan Schneider <Jan.Schneider1997@gmail.com>
2023-10-13 12:16:12 +0200

    Switched to sequential processing for the seeds since Ax is not using Bayesian optimization anyway atm

--- pg_subspaces/scripts/tune.py
+++ pg_subspaces/scripts/tune.py
@@ -14,22 +14,24 @@ from pg_subspaces.scripts.train import train
 logger = logging.getLogger(__name__)
 
 
-def worker(cfg: omegaconf.DictConfig, seed: int) -> float:
+def worker(cfg: omegaconf.DictConfig, seed: int, working_directory: Path) -> float:
     cfg = copy.deepcopy(cfg)
     cfg.seed = seed
     # Hack: The sweeper cannot handle list-type parameters, so pass a single number and convert it to a list
     cfg.algorithm.algorithm.policy_kwargs.net_arch = omegaconf.ListConfig(
         [cfg.algorithm.algorithm.policy_kwargs.net_arch]
     )
-    # cfg.run_log_dir = Path(cfg.run_log_dir) / str(seed)
     orig_dir = Path.cwd()
-    seed_dir = Path(str(seed))
-    seed_dir.mkdir()
-    os.chdir(seed_dir)
-    train(cfg)
-    _, event_accumulator = create_event_accumulators([Path("tensorboard")])[0]
+    working_directory.mkdir()
+    try:
+        os.chdir(working_directory)
+        train(cfg)
+    finally:
+        os.chdir(orig_dir)
+    _, event_accumulator = create_event_accumulators(
+        [working_directory / "tensorboard"]
+    )[0]
     scalar = read_scalar(event_accumulator, "eval/mean_reward")
-    os.chdir(orig_dir)
     return -sum(v.value for s, v in scalar.items() if s > 0)
 
 
@@ -37,8 +39,10 @@ def worker(cfg: omegaconf.DictConfig, seed: int) -> float:
 def tune_hyperparams(cfg: omegaconf.DictConfig):
     num_seeds = 5
     seeds = range(num_seeds)
-    results = Parallel(n_jobs=num_seeds, backend="loky")(
-        delayed(worker)(cfg, seed) for seed in seeds
+    results = Parallel(n_jobs=num_seeds, backend="sequential")(
+        # Need to pass the working directory here since otherwise loky messes up the paths for some reason
+        delayed(worker)(cfg, seed, Path.cwd().absolute() / str(seed))
+        for seed in seeds
     )
 
     return np.mean(results)
