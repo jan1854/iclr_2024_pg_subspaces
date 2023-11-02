2a0b97594098c296606cf1f79b363b46d6532f8a
Jan Schneider <Jan.Schneider1997@gmail.com>
2023-11-02 11:22:49 +0100

    Added high curvature analysis plots for the ablations

--- pg_subspaces/scripts/create_hc_analysis_plots.py
+++ pg_subspaces/scripts/create_hc_analysis_plots.py
@@ -13,9 +13,7 @@ logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
-out_dir = Path(__file__).parents[2] / "out"
-
-PLOT_CONFIGS = {
+PLOT_CONFIGS_SINGLE_RUN = {
     "gradient_subspace_fraction_precise_vs_low_sample_hessian": {
         # "title": "Subspace frac., precise vs. low-sample Hessian",
         "out_dir": "gradient_subspace_fraction",
@@ -66,42 +64,42 @@ PLOT_CONFIGS = {
         "ymax": 1.0,
         "smoothing_weight": 0.0,
     },
-    "true_gradient_subspace_fraction": {
-        # "title": "True gradient subspace fraction",
-        "out_dir": "gradient_subspace_fraction",
-        "keys": [
-            "gradient_subspace_fraction_001evs/true_gradient",
-            "gradient_subspace_fraction_002evs/true_gradient",
-            "gradient_subspace_fraction_005evs/true_gradient",
-            "gradient_subspace_fraction_010evs/true_gradient",
-            "gradient_subspace_fraction_020evs/true_gradient",
-            "gradient_subspace_fraction_050evs/true_gradient",
-            "gradient_subspace_fraction_100evs/true_gradient",
-        ],
-        "legend": ["1 EV", "2 EVs", "5 EVs", "10 EVs", "20 EVs", "50 EVs", "100 EVs"],
-        "ylabel": "Gradient subspace fraction",
-        "ymin": 0.0,
-        "ymax": 1.0,
-        "smoothing_weight": 0.0,
-    },
-    "estimated_gradient_subspace_fraction": {
-        # "title": "Est. gradient subspace fraction",
-        "out_dir": "gradient_subspace_fraction",
-        "keys": [
-            "gradient_subspace_fraction_001evs/estimated_gradient",
-            "gradient_subspace_fraction_002evs/estimated_gradient",
-            "gradient_subspace_fraction_005evs/estimated_gradient",
-            "gradient_subspace_fraction_010evs/estimated_gradient",
-            "gradient_subspace_fraction_020evs/estimated_gradient",
-            "gradient_subspace_fraction_050evs/estimated_gradient",
-            "gradient_subspace_fraction_100evs/estimated_gradient",
-        ],
-        "legend": ["1 EV", "2 EVs", "5 EVs", "10 EVs", "20 EVs", "50 EVs", "100 EVs"],
-        "ylabel": "Gradient subspace fraction",
-        "ymin": 0.0,
-        "ymax": 1.0,
-        "smoothing_weight": 0.0,
-    },
+    # "true_gradient_subspace_fraction": {
+    #     # "title": "True gradient subspace fraction",
+    #     "out_dir": "gradient_subspace_fraction",
+    #     "keys": [
+    #         "gradient_subspace_fraction_001evs/true_gradient",
+    #         "gradient_subspace_fraction_002evs/true_gradient",
+    #         "gradient_subspace_fraction_005evs/true_gradient",
+    #         "gradient_subspace_fraction_010evs/true_gradient",
+    #         "gradient_subspace_fraction_020evs/true_gradient",
+    #         "gradient_subspace_fraction_050evs/true_gradient",
+    #         "gradient_subspace_fraction_100evs/true_gradient",
+    #     ],
+    #     "legend": ["1 EV", "2 EVs", "5 EVs", "10 EVs", "20 EVs", "50 EVs", "100 EVs"],
+    #     "ylabel": "Gradient subspace fraction",
+    #     "ymin": 0.0,
+    #     "ymax": 1.0,
+    #     "smoothing_weight": 0.0,
+    # },
+    # "estimated_gradient_subspace_fraction": {
+    #     # "title": "Est. gradient subspace fraction",
+    #     "out_dir": "gradient_subspace_fraction",
+    #     "keys": [
+    #         "gradient_subspace_fraction_001evs/estimated_gradient",
+    #         "gradient_subspace_fraction_002evs/estimated_gradient",
+    #         "gradient_subspace_fraction_005evs/estimated_gradient",
+    #         "gradient_subspace_fraction_010evs/estimated_gradient",
+    #         "gradient_subspace_fraction_020evs/estimated_gradient",
+    #         "gradient_subspace_fraction_050evs/estimated_gradient",
+    #         "gradient_subspace_fraction_100evs/estimated_gradient",
+    #     ],
+    #     "legend": ["1 EV", "2 EVs", "5 EVs", "10 EVs", "20 EVs", "50 EVs", "100 EVs"],
+    #     "ylabel": "Gradient subspace fraction",
+    #     "ymin": 0.0,
+    #     "ymax": 1.0,
+    #     "smoothing_weight": 0.0,
+    # },
     "subspace_overlap_0100000_precise_vs_low_sample_hessian": {
         # "title": "Subspace overlap, precise vs. low-sample Hessian, $t_1 = 10^{5}$",
         "out_dir": "subspace_overlap",
@@ -182,8 +180,59 @@ PLOT_CONFIGS = {
     },
 }
 
+PLOT_CONFIGS_MULTIPLE_RUNS = {
+    "gradient_subspace_fraction_true_010evs": {
+        "out_dir": "gradient_subspace_fraction",
+        "key": "gradient_subspace_fraction_010evs/true_gradient",
+        "ylabel": "Gradient subspace fraction",
+        "ymin": 0.0,
+        "ymax": 1.0,
+        "smoothing_weight": 0.0,
+    },
+    "gradient_subspace_fraction_true_100evs": {
+        "out_dir": "gradient_subspace_fraction",
+        "key": "gradient_subspace_fraction_100evs/true_gradient",
+        "ylabel": "Gradient subspace fraction",
+        "ymin": 0.0,
+        "ymax": 1.0,
+        "smoothing_weight": 0.0,
+    },
+    "subspace_overlap_010evs_checkpoint_0010000": {
+        "out_dir": "gradient_subspace_fraction",
+        "key": "overlaps_top100_checkpoint0010000",
+        "ylabel": "Subspace overlap",
+        "ymin": 0.0,
+        "ymax": 1.0,
+        "smoothing_weight": 0.0,
+    },
+    "subspace_overlap_100evs_checkpoint_0010000": {
+        "out_dir": "gradient_subspace_fraction",
+        "key": "overlaps_top100_checkpoint0010000",
+        "ylabel": "Subspace overlap",
+        "ymin": 0.0,
+        "ymax": 1.0,
+        "smoothing_weight": 0.0,
+    },
+    "subspace_overlap_010evs_checkpoint_0100000": {
+        "out_dir": "gradient_subspace_fraction",
+        "key": "overlaps_top100_checkpoint0100000",
+        "ylabel": "Subspace overlap",
+        "ymin": 0.0,
+        "ymax": 1.0,
+        "smoothing_weight": 0.0,
+    },
+    "subspace_overlap_100evs_checkpoint_0100000": {
+        "out_dir": "gradient_subspace_fraction",
+        "key": "overlaps_top100_checkpoint0100000",
+        "ylabel": "Subspace overlap",
+        "ymin": 0.0,
+        "ymax": 1.0,
+        "smoothing_weight": 0.0,
+    },
+}
 
-def worker(
+
+def worker_single_run(
     log_path: Path,
     legend: Optional[Sequence[str]],
     title: str,
@@ -232,12 +281,66 @@ def worker(
         )
 
 
+def worker_multiple_runs(
+    log_paths: Sequence[Path],
+    legend: Optional[Sequence[str]],
+    title: str,
+    xlabel: str,
+    ylabel: str,
+    xlimits: Tuple[float, float],
+    ylimits: Tuple[float, float],
+    xaxis_log: bool,
+    key: Sequence[str],
+    smoothing_weight: float,
+    only_complete_steps: bool,
+    separate_legend: bool,
+    num_same_color_plots: int,
+    fontsize: int,
+    linewidth: float,
+    fill_in_data: Dict[int, float],
+    out: Path,
+    annotations: Dict[int, str],
+):
+    from pg_subspaces.scripts.create_plots import create_plots
+
+    try:
+        create_plots(
+            log_paths,
+            legend,
+            title,
+            xlabel,
+            ylabel,
+            xlimits,
+            ylimits,
+            xaxis_log,
+            key,
+            smoothing_weight,
+            only_complete_steps,
+            separate_legend,
+            num_same_color_plots,
+            fontsize,
+            linewidth,
+            fill_in_data,
+            out,
+            annotations,
+        )
+    except Exception as e:
+        logger.warning(
+            f"Could not create plot {out.relative_to(out.parents[2])}, got exception {e}"
+        )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("log_dir", type=str)
     parser.add_argument("envs", type=str, nargs="*", default=["*"])
+    parser.add_argument("--out", type=str)
     args = parser.parse_args()
     log_dir = Path(args.log_dir)
+    if args.out is not None:
+        out_dir = Path(args.out)
+    else:
+        out_dir = Path(__file__).parents[2] / "out"
     with (Path(__file__).parent / "res" / "run_configs.yaml").open(
         "r"
     ) as run_configs_file:
@@ -257,7 +360,7 @@ if __name__ == "__main__":
                 env_file_name = env_name[:-3].lower().replace("-", "_")
                 if not env_name.startswith("dmc"):
                     env_file_name = "gym_" + env_file_name
-                for out_filename, plot_config in PLOT_CONFIGS.items():
+                for out_filename, plot_config in PLOT_CONFIGS_SINGLE_RUN.items():
                     for loss_type, loss_type_short in [
                         ("combined_loss", "combined"),
                         ("policy_loss", "policy"),
@@ -305,7 +408,7 @@ if __name__ == "__main__":
                         )
                         results.append(
                             pool.apply_async(
-                                worker,
+                                worker_single_run,
                                 (
                                     curr_log_dir,
                                     plot_config["legend"],
@@ -316,7 +419,7 @@ if __name__ == "__main__":
                                     (plot_config.get("ymin"), plot_config.get("ymax")),
                                     False,
                                     keys,
-                                    0.3,
+                                    plot_config.get("smoothing_weight", 0.0),
                                     True,
                                     plot_config.get("num_same_color_plots", 1),
                                     plot_config.get("marker"),
@@ -328,5 +431,64 @@ if __name__ == "__main__":
                                 ),
                             )
                         )
+
+        for experiment_name, experiment_config in experiment_configs.items():
+            for env_name, run_config in run_configs.items():
+                if env_name not in args.envs and "*" not in args.envs:
+                    continue
+                env_file_name = env_name[:-3].lower().replace("-", "_")
+                if not env_name.startswith("dmc"):
+                    env_file_name = "gym_" + env_file_name
+                log_paths = [
+                    log_dir / "training" / env_name / run_config["log_dirs"][algo_name]
+                    for algo_name in experiment_config.keys()
+                    if algo_name in run_config["log_dirs"]
+                ]
+                analysis_run_ids = [
+                    a_id for a_id in run_config["analysis_run_ids"] if a_id != "default"
+                ] + ["default"]
+                for out_filename, plot_config in PLOT_CONFIGS_MULTIPLE_RUNS.items():
+                    for loss_type, loss_type_short in [
+                        ("combined_loss", "combined"),
+                        ("policy_loss", "policy"),
+                        ("value_function_loss", "vf"),
+                    ]:
+                        out_path = (
+                            out_dir
+                            / experiment_name
+                            / plot_config["out_dir"]
+                            / loss_type
+                            / f"{env_file_name}_{out_filename}_{loss_type_short}.pdf"
+                        )
+                        keys = [
+                            f"high_curvature_subspace_analysis/{a_id}/{plot_config['key']}/{loss_type}"
+                            for a_id in analysis_run_ids
+                        ]
+                        results.append(
+                            pool.apply_async(
+                                worker_multiple_runs,
+                                (
+                                    log_paths,
+                                    list(experiment_config.values()),
+                                    None,
+                                    plot_config.get("xlabel", "Environment steps"),
+                                    plot_config["ylabel"],
+                                    (run_config.get("xmin", 0), run_config.get("xmax")),
+                                    (plot_config.get("ymin"), plot_config.get("ymax")),
+                                    False,
+                                    keys,
+                                    plot_config.get("smoothing_weight", 0.0),
+                                    True,
+                                    True,
+                                    plot_config.get("num_same_color_plots", 1),
+                                    plot_config.get("fontsize", 22),
+                                    plot_config.get("linewidth", 2.5),
+                                    plot_config.get("fill_in_data", {}),
+                                    out_path,
+                                    plot_config.get("annotations", {}),
+                                ),
+                            )
+                        )
+
         for res in tqdm(results):
             res.get()
