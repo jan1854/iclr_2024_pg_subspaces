492ba886a46c07c3812b1a333b4fa07a22af2f7c
Jan Schneider <Jan.Schneider1997@gmail.com>
2023-12-04 10:27:18 +0100

    Moved the checkpointing test to test_util as the checkpointing is not part of sb3_utils

--- test/test_util.py
+++ test/test_util.py
@@ -1,13 +1,22 @@
+import re
+import tempfile
+from pathlib import Path
+
 import gym
 import numpy as np
 import pytest
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
+from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.vec_env import DummyVecEnv
 
 from pg_subspaces.analysis.util import (
     evaluate_agent_returns,
 )
+from pg_subspaces.callbacks.custom_checkpoint_callback import CustomCheckpointCallback
 from pg_subspaces.metrics.tensorboard_logs import merge_dicts
+from pg_subspaces.sb3_utils.common.replay_buffer_diff_checkpointer import (
+    ReplayBufferDiffCheckpointer,
+)
 
 
 class DummyEnv(gym.Env):
@@ -83,3 +92,60 @@ def test_evaluate_agent_returns():
     assert eval_result_episodes.rewards_discounted.item() == pytest.approx(
         gt_value_discounted
     )
+
+
+def test_replay_buffer_checkpointing():
+    env = DummyVecEnv([lambda: gym.make("Pendulum-v1")])
+    algo = SAC(
+        "MlpPolicy",
+        env,
+        buffer_size=20,
+        policy_kwargs={"net_arch": [32, 32]},
+        device="cpu",
+    )
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        callbacks = [
+            CustomCheckpointCallback(19, [], tempdir, "sac", True),
+            CheckpointCallback(19, tempdir, "sac", True),
+        ]
+        algo.learn(500, callbacks)
+
+        for checkpoint in tempdir.glob("sac_[0-9]*_steps.zip"):
+            step = int(re.search("_[0-9]+_", checkpoint.name).group()[1:-1])
+            algo = SAC.load(checkpoint)
+            algo.load_replay_buffer(tempdir / f"sac_replay_buffer_{step}_steps.pkl")
+            replay_buffer_checkpointer = ReplayBufferDiffCheckpointer(
+                algo, "sac", tempdir
+            )
+
+            # The replay buffer loaded with the regular sb3 checkpointing
+            obs_complete = algo.replay_buffer.observations.copy()
+            act_complete = algo.replay_buffer.actions.copy()
+            next_obs_complete = algo.replay_buffer.next_observations.copy()
+            reward_complete = algo.replay_buffer.rewards.copy()
+            done_complete = algo.replay_buffer.dones.copy()
+            pos_complete = algo.replay_buffer.pos
+            full_complete = algo.replay_buffer.full
+
+            # Make sure that the replay buffer is modified
+            algo.replay_buffer.observations = np.zeros_like(
+                algo.replay_buffer.observations
+            )
+            algo.replay_buffer.actions = np.zeros_like(algo.replay_buffer.actions)
+            algo.replay_buffer.next_observations = np.zeros_like(
+                algo.replay_buffer.next_observations
+            )
+            algo.replay_buffer.rewards = np.zeros_like(algo.replay_buffer.rewards)
+            algo.replay_buffer.dones = np.zeros_like(algo.replay_buffer.dones)
+            algo.replay_buffer.pos = -1
+            algo.replay_buffer.full = None
+
+            replay_buffer_checkpointer.load(step)
+            assert np.all(algo.replay_buffer.observations == obs_complete)
+            assert np.all(algo.replay_buffer.actions == act_complete)
+            assert np.all(algo.replay_buffer.next_observations == next_obs_complete)
+            assert np.all(algo.replay_buffer.rewards == reward_complete)
+            assert np.all(algo.replay_buffer.dones == done_complete)
+            assert algo.replay_buffer.pos == pos_complete
+            assert algo.replay_buffer.full == full_complete
